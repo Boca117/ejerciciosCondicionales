873c02578d812bcfeae8203cb79f575ed1376c33
Juan Bocanegra <jbocanegrae@unal.edu.co>
2024-07-11 10:56:13 -0500

    se deja todo sin comentar y para entregar

--- main.js
+++ main.js
@@ -1,65 +1,65 @@
 //Punto 1//
-/*
+
 let numeroA = prompt("Ingresa el primer numero")
 let numeroB = prompt("Ingresa el segundo numero")
 
 if (numeroA > numeroB) {
-    alert("El primer numero es mayor")
+    console.log("El primer numero es mayor")
 
 }
-*/
+
 
 //Punto 2//
 
-/*
-let numeroA = prompt("Ingresa el primer numero")
-let numeroB = prompt("Ingresa el segundo numero")
 
-if (numeroA == numeroB) {
-    alert("Los dos numeros son iguales")
+let numeroE = prompt("Ingresa el primer numero")
+let numeroD = prompt("Ingresa el segundo numero")
+
+if (numeroE == numeroD) {
+    console.log("Los dos numeros son iguales")
    
 }
 else {
-        alert("los numeros son diferentes")
+    console.log("los numeros son diferentes")
 }
-*/
+
 
 //Punto 3//
-/*
-let numeroA = prompt("Ingresa el primer numero")
-let numeroB = prompt("Ingresa el segundo numero")
+
+let primerNumero = prompt("Ingresa el primer numero")
+let segundoNumero = prompt("Ingresa el segundo numero")
 
 
-if (numeroA > numeroB) {
-    alert("El primer numero es mayor")
+if (primerNumero > segundoNumero) {
+    console.log("El primer numero es mayor")
 }
-else if (numeroA < numeroB) {
-    alert("El segundo numero es mayor")
+else if (primerNumero < segundoNumero) {
+    console.log("El segundo numero es mayor")
 }
 else {
-    alert("Los dos numeros son iguales")
+    console.log("Los dos numeros son iguales")
 }
-*/
+
 //Punto 4//
 
-/*
-let numeroA = prompt("Ingresa el primer numero")
-let numeroB = prompt("Ingresa el segundo numero")
-let numeroC = prompt("Ingresa el tercer numero")
 
-if (numeroA < numeroB && numeroA < numeroC) {
-    alert("El primer numero es el menor")
+let numero1 = prompt("Ingresa el primer numero")
+let numero2 = prompt("Ingresa el segundo numero")
+let numero3 = prompt("Ingresa el tercer numero")
+
+if (numero1 < numero2 && numero1 < numero3) {
+    console.log("El primer numero es el menor")
 }
-else if (numeroB < numeroA && numeroB < numeroC) {
-    alert("El segundo numero es el menor")
+else if (numero2 < numero1 && numero2 < numero3) {
+    console.log("El segundo numero es el menor")
 }
-else if (numeroC < numeroA && numeroC < numeroB) {
-    alert("El tercer numero es el menor")
+else if (numero3 < numero1 && numero3 < numero2) {
+    console.log("El tercer numero es el menor")
 }
-*/
+
 
 //Punto 5//
-/*
+
 let persona1 = {
     nombre: "Juan",
     edad: 18,
@@ -73,24 +73,24 @@ let persona2 = {
 }
 
 if (persona1.altura > persona2.altura) {
-    alert("La persona 1 es mas alta")
+    console.log("La persona 1 es mas alta")
 }
 else if (persona2.altura > persona1.altura) {
-    alert("La persona 2 es mas alta")
+   console.log("La persona 2 es mas alta")
 }
 
 
 
 if (persona1.edad > persona2.edad) {
-    alert("La persona 1 es mayor")
+    console.log("La persona 1 es mayor")
 }
 else if (persona2.edad > persona1.edad) {
-    alert("La persona 2 es mayor")
+    console.log("La persona 2 es mayor")
 }
-*/
+
 
 //Punto 6//
-/*
+
 let persona = {
     nombre: prompt("Ingresa tu nombre"),
     edad: prompt("Ingresa tu edad"),
@@ -99,17 +99,17 @@ let persona = {
 }
 
 if (persona.vision >= 8 && persona.edad >= 18 && persona.altura > 150) {
-    alert("La persona esta capacitada para conducir")    
+    console.log("La persona esta capacitada para conducir")    
 }
 else {
-    alert("La persona NO esta capacitada para conducir")
+    console.log("La persona NO esta capacitada para conducir")
 }
-*/
+
 
 //Punto 7//
 
-/*
-let nombre = prompt("Ingresa tu nombre")
+
+let nombreUsuario = prompt("Ingresa tu nombre")
 let pase = prompt("Ingresa tu tipo de pase (vip o normal)")
 let entrada = prompt("Tienes entrada?")
 
@@ -117,80 +117,80 @@ let tipoPase = ["vip", "normal"]
 let conEntrada = ["si", "s", true]
 let sinEntrada = ["no", "n", false]
 
-nombre = nombre.toLowerCase()
+nombreUsuario = nombreUsuario.toLowerCase()
 pase = pase.toLowerCase()
 entrada = entrada.toLowerCase()
 
 switch (true) {
-    case (nombre === "juan"):
-        alert("Bienvenido, tiene entrada libre Juan!!")
+    case (nombreUsuario === "juan"):
+        console.log("Bienvenido, tiene entrada libre Juan!!")
         break
 
         
     case (pase === tipoPase[1] && (conEntrada.includes(entrada))):
         let utilizar = prompt("Deseas utilizar tu entrada")
             if (conEntrada.includes(utilizar)) {
-                alert("Bienvenido")
+                console.log("Bienvenido")
             }
             else {
-                alert("Hasta la proxima!!")
+                console.log("Hasta la proxima!!")
             }
         break
     
-    case (pase === tipoPase[1] && (sinEntrada.includes(entrada)) && nombre !== "juan"):
+    case (pase === tipoPase[1] && (sinEntrada.includes(entrada)) && nombreUsuario !== "juan"):
         let compra = prompt("Deseas comprar una entrada")
             if (conEntrada.includes(compra)) {
                 let saldo = prompt("Ingresa tu saldo")
                 if (saldo >= 1000) {
-                    alert("Entrada comprada correctamente, Bienvenido")    
+                   console.log("Entrada comprada correctamente, Bienvenido")    
                 }
                 else {
-                    alert("No tienes saldo suficiente para comprar una entrada, Hasta la proxima!!")
+                    console.log("No tienes saldo suficiente para comprar una entrada, Hasta la proxima!!")
                 }
             }
             else {
-                alert("Hasta la proxima!!")
+                console.log("Hasta la proxima!!")
             }
 
         break    
     default:
-        alert("Entrada no válida o pase no reconocido.")
+        console.log("Entrada no válida o pase no reconocido.")
         break
 }
-*/
+
 
 //Punto 8//
-/*
+
 let numeroIncognita = 6
 let numeroIngresado = prompt("Ingresa un numero del 1 al 10, tienes 3 intentos para adivinar")
 
 
 switch (true) {
     case (numeroIngresado == numeroIncognita):
-        alert("Ganaste")
+        console.log("Ganaste")
         break
     case (numeroIngresado < numeroIncognita):
         numeroIngresado = prompt("El numero ingresado es menor intenta de nuevo, te quedan 2 intentos")
 
         if (numeroIngresado == numeroIncognita) {
-            alert("Ganaste")
+            console.log("Ganaste")
         }    
         else if (numeroIngresado < numeroIncognita) {
             numeroIngresado = prompt("El numero ingresado es menor intenta de nuevo, te quedan 1 intento")
             if (numeroIngresado == numeroIncognita) {
-                alert("Ganaste")
+                console.log("Ganaste")
             }
             else {
-                alert("Perdiste, el numero era: " + numeroIncognita)
+                console.log("Perdiste, el numero era: " + numeroIncognita)
             }
         }
         else if (numeroIngresado > numeroIncognita) {
             numeroIngresado = prompt("El numero ingresado es mayor intenta de nuevo, te queda 1 intento")
             if (numeroIngresado == numeroIncognita) {
-                alert("Ganaste")
+                console.log("Ganaste")
             }
             else {
-                alert("Perdiste, el numero era: " + numeroIncognita)
+                console.log("Perdiste, el numero era: " + numeroIncognita)
             }
 
         }
@@ -200,36 +200,36 @@ switch (true) {
         numeroIngresado = prompt("El numero  ingresado es mayor intenta de nuevo, te quedan 2 intentos")
 
         if (numeroIngresado == numeroIncognita) {
-            alert("Ganaste")
+            console.log("Ganaste")
         }    
         else if (numeroIngresado < numeroIncognita) {
             numeroIngresado = prompt("El numero ingresado es menor intenta de nuevo, te quedan 1 intento")
             if (numeroIngresado == numeroIncognita) {
-                alert("Ganaste")
+                console.log("Ganaste")
             }
             else {
-                alert("Perdiste, el numero era: " + numeroIncognita)
+                console.log("Perdiste, el numero era: " + numeroIncognita)
             }
         }
         else if (numeroIngresado > numeroIncognita) {
             numeroIngresado = prompt("El numero ingresado es mayor intenta de nuevo, te queda 1 intento")
             if (numeroIngresado == numeroIncognita) {
-                alert("Ganaste")
+                console.log("Ganaste")
             }
             else {
-                alert("Perdiste, el numero era: " + numeroIncognita)
+                console.log("Perdiste, el numero era: " + numeroIncognita)
             }
         }
         
         break
 
     default:
-        alert("Perdiste, el numero era: " + numeroIncognita)
+        console.log("Perdiste, el numero era: " + numeroIncognita)
         break
 }
-*/
+
 //Punto 9//
-/*
+
 let edad = prompt("Ingresa tu edad")
 
 switch (true) {
@@ -249,10 +249,10 @@ switch (true) {
         alert("De verdad tienes mas de 100 años :O ?")
         break
 }
-*/
+
 
 //Punto 10//
-/*
+
 let jugador1 = prompt("Jugador 1, ingresa PIEDRA, PAPEL o TIJERA")
 let jugador2 = prompt("Jugador 2, ingresa PIEDRA, PAPEL o TIJERA")
 let opciones = ["PIEDRA", "PAPEL", "TIJERA"]
@@ -288,42 +288,42 @@ switch (true) {
         alert("Algun jugador hizo trampa >:(")
         break
 }
-*/
+
 //Punto 11//
-/*
+
 let color = prompt("Ingresa un color")
 color = color.toLowerCase()
 
 switch (true) {
     case (color == "negro" || color == "blanco"):
-        alert("Falta de color")
+        console.log("Falta de color")
         break
     case (color == "verde"):
-        alert("El color de la naturaleza")
+        console.log("El color de la naturaleza")
         break
     case (color == "azul"):
-        alert("El color del agua")
+        console.log("El color del agua")
         break
     case (color == "amarillo"):
-        alert("El color del sol")
+        console.log("El color del sol")
         break
     case (color == "rojo"):
-        alert("El color del fuego")
+        console.log("El color del fuego")
         break
     case (color == "marron"):
-        alert("El color de la tierra")
+        console.log("El color de la tierra")
         break
     default:
-        alert("Excelente eleccion no lo teniamos pensado")
+        console.log("Excelente eleccion no lo teniamos pensado")
         break
 }
-*/
+
 
 //Punto 12//
 
-/*
-let numeroA = Number(prompt(("Ingresa el primer numero")))
-let numeroB = Number(prompt("Ingresa el segundo numero"))
+
+let primerDigito = Number(prompt(("Ingresa el primer numero")))
+let segundoDigito = Number(prompt("Ingresa el segundo numero"))
 let operacion = prompt("Ingresa la operacion (suma, resta, multiplicacion o division)")
 operacion = operacion.toLowerCase()
 
@@ -331,26 +331,26 @@ let operaciones = ["suma", "resta", "multiplicacion", "division"]
 
 
 if (operacion == operaciones[0]) {
-    alert("El resultado es" + (numeroA + numeroB))
+    console.log("El resultado es" + (primerDigito + segundoDigito))
 }
 else if (operacion == operaciones[1]) {
-    alert("El resultado es" + (numeroA - numeroB))
+    console.log("El resultado es" + (primerDigito - segundoDigito))
 }
 else if (operacion == operaciones[2]) {
-    alert("El resultado es" + (numeroA * numeroB))
+    console.log("El resultado es" + (primerDigito * segundoDigito))
 }
 else if (operacion == operaciones[3]) {
-    if (numeroB != 0) {
-        alert("El resultado es" + (numeroA / numeroB))
+    if (segundoDigito != 0) {
+        console.log("El resultado es" + (primerDigito / segundoDigito))
     }
     else {
-        alert("ERROR, no se puede dividir entre 0")
+        console.log("ERROR, no se puede dividir entre 0")
     }
 }
 else {
-    alert("Ingresa una operacion valida")
+    console.log("Ingresa una operacion valida")
 }
-*/
+
 
 //Punto 13//
 
